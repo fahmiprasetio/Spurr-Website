d94ea330b3574b81a358955897f7c01e7d9d2db5
Spurr Dev <nfahmi086@gmail.com>
2026-05-06 23:59:20 +0700

    ubah

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -67,7 +67,7 @@ export default function Hero({
         />
       </div>
 
-      <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-8 text-center">
+      <div className="relative z-10 max-w-6xl mx-auto px-6 lg:px-8 text-center">
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
